9bbc0601e16b16df1622d0d24a0f57c52d741d9f
devitalo <italo_ariel_dev77@outlook.com>
2025-01-08 01:03:01 -0300

    first commit

---
+++ src/services/NSE.identidade.API/NSE.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,6 @@
+﻿namespace NSE.Identidade.API.Controllers
+{
+    public class AuthController
+    {
+    }
+}
--- src/services/NSE.identidade.API/NSE.Identidade.API/Program.cs
+++ src/services/NSE.identidade.API/NSE.Identidade.API/Program.cs
@@ -4,10 +4,8 @@ using NSE.Identidade.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -24,7 +22,7 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
