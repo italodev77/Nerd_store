7e4ba9d92302d2bac00e075e6a51b36ccbdb3a23
devitalo <italo_ariel_dev77@outlook.com>
2025-01-09 01:18:09 -0300

    feat: implementação da controller de autenticação e registro do user

--- src/services/NSE.identidade.API/NSE.Identidade.API/Controllers/AuthController.cs
+++ src/services/NSE.identidade.API/NSE.Identidade.API/Controllers/AuthController.cs
@@ -1,6 +1,54 @@
-﻿namespace NSE.Identidade.API.Controllers
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identidade.API.Models;
+
+namespace NSE.Identidade.API.Controllers
 {
-    public class AuthController
+    [Route("api/identidade")]
+    public class AuthController: ControllerBase
     {
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Register(UsuarioRegistro usuarioRegistro)
+        {
+            if(!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true
+            };
+            var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
+            //persistence é igual ao se usuário vai ser lembrado
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+        [HttpPost("autenticar")]
+        public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha, isPersistent: false, true);
+            
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
     }
 }
--- src/services/NSE.identidade.API/NSE.Identidade.API/Program.cs
+++ src/services/NSE.identidade.API/NSE.Identidade.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using NSE.Identidade.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+{
+    Title = "NerdStore Enterprise Identity API",
+    Description = "Está api é para autenticação e registro de usuários para a plataforma NSE",
+    Contact = new OpenApiContact() { Name = "Italo Ariel", Email = "italo_ariel_dev77@outlook.com" },
+    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+}));
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
